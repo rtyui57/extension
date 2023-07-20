e02032eca851e525bc9655507af4ddbf3cc21487
Ramon Ramos <ramon@ual.com>
2023-07-20 18:27:09 +0200

    Funciona

--- contentScript.js
+++ contentScript.js
@@ -1,23 +1,33 @@
-if (typeof init === 'undefined') {
-    const init = function () {
-        const injectElement = document.createElement('div');
-        injectElement.className = 'rustyZone-element';
-        injectElement.innerHTML = 'Hello From the Rusty Zone Element';
-        document.body.appendChild(injectElement);
+if (typeof init === 'undefined' & !exists()) {
+  appendDownloaderButton();
+  console.log("Se ejecutó la función")
+}
 
-        const hostEle = document.createElement('div');
-        hostEle.className = 'rustyZone-element-host';
-        hostEle.innerHTML = 'Hello From the Rusty Zone Element';
-        document.body.appendChild(hostEle);
+function appendDownloaderButton() {
+  const menuTop = document.getElementsByClassName("SidebarMenu__sideMenuTop--vJNxy")[0];
+  const xpath = '//*[@id="react-app"]/div/div/div[3]/div/div[1]/div[2]/div[1]/div[1]/button';
+  const buttonElement = document.evaluate(xpath, document, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null).singleNodeValue;
+  if (menuTop) {
+    const nuevo = buttonElement.cloneNode(true);
+    nuevo.textContent = "Download ZIP"
+    nuevo.id = "zipDonwloader"
+    nuevo.style.backgroundColor = 'red';
+    const baseUrl = extractBaseUrl(window.location.href);
+    nuevo.addEventListener('click', function () {
+      chrome.runtime.sendMessage({ action: 'createNewTab', url: baseUrl });
+    });
+    menuTop.appendChild(nuevo);
+  } else {
+    console.log("No se ha encontrado el menu donde crear el botón")
+  }
+}
 
-        //Using Shadow Root
-        var host = document.querySelector('.rustyZone-element-host');
-        var root = host.attachShadow({ mode: 'open' }); // Create a Shadow Root
-        var div = document.createElement('div');
-        div.className = 'div root-class';
-        div.innerHTML = '<style>.div{border:3px solid blue;margin:10px;padding:10px;width:200px;}</style>'
-            + 'Hello From the Rusty Zone Shadow Root Element';
-        root.appendChild(div);
-    }
-    init();
-}  
+function extractBaseUrl(url) {
+  const regex = /^(https?:\/\/www\.thingiverse\.com\/thing:\d{7})/;
+  const matched = url.match(regex);
+  return matched ? matched[1] + "/zip" : null;
+}
+
+function exists() {
+  return document.querySelector("#zipDonwloader");
+}
